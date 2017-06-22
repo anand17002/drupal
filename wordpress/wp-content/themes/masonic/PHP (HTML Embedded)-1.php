fb2a7fe570f2d3c26eca127f5e12488dc29bea26
anand17002 <email2anand54@gmail.com>
2017-06-22 04:34:53 -0700

    wordpress commited

---
+++ wordpress/wp-content/themes/masonic/PHP (HTML Embedded)-1.php
@@ -0,0 +1,121 @@
+<?php
+$var = $_SERVER['PHP_SELF'];
+$arr  = explode("/",$var,-1);
+$base_path = implode("/",$arr); ?>
+
+<table align="center">
+      <tbody>
+        <tr>
+          <td align="center" valign="top" colspan="2"><strong>Volume1 </strong></td>
+        </tr>
+      <tr>
+          <td align="left" valign="top">Sl No.        </td>
+            <td align="center" valign="middle"> DESCRIPTION</td>
+        </tr>
+       
+          <tr>
+          <td align="left" valign="top">1</td>
+            <td align="left" valign="top"> <a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement1.xls" target="_blank">Net Profit/Loss (-) in CPSEs Under Different Ministries/Departments</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">2</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement2.xls" target="_blank"> Dividend Declared by CPSEs (Cognate Group Wise)</a> </td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">3</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement3.xls" target="_blank"> Net Profit of Profit making CPSEs (Cognate Group Wise)</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">4</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement4.xls" target="_blank">Net Loss (-) of Loss making CPSEs (Cognate Group Wise)</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">5</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement5.xls" target="_blank">Cash Loss (-) of Loss making CPSEs  during 2014-15 </a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">6</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement6.xls" target="_blank">CPSE Wise Net Profit/Loss(-) 'Taken Over' Sick CPSEs</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">7</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement7.xls" target="_blank"> Investment (Financial) in CPSEs Under Different Ministries/Deptt. as on 31.3.2015</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">8</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement8.xls" target="_blank"> Details of participation in Equity of CPSEs by Different Parties as on 31.3.2015</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">9</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement9.xls" target="_blank">Details of Short Term Loans of CPSEs as on 31.3.2015</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">10</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement10.xls" target="_blank">Details of Long Term Borrowings of CPSEs as on 31.3.2015</a></td>
+        </tr>
+  <tr>
+          <td align="left" valign="top">11</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement11.xls" target="_blank">State Wise CPSEs WITH Gross Block and Employment in CPSEs During Last Three Years</a></td>
+        </tr>
+          <tr>
+          <td align="left" valign="top">12</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement12.xls" target="_blank">Ranking of CPSEs in terms of Pre-Tax Profit/Loss(-) (PBEET) in 2014-15</a></td>
+        </tr>
+          <tr>
+          <td align="left" valign="top">13</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement13.xls" target="_blank"> Ranking of CPSEs in terms of Net Sales in 2014-15</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">14</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement14.xls" target="_blank"></a> Ranking of CPSEs in terms of Employment in </a> </td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">15</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement15.xls" target="_blank">Ranking of CPSEs in terms of Gross Block in </a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">16</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement16.xls" target="_blank">Capacity Utilisation in Manufacturing CPSEs</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">17</td>
+            <td align="left" valign="top"> <a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement17.xls" target="_blank">Expenditure on Research and Development in CPSEs</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">18</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement18.xls" target="_blank">Components of Internal Resources of CPSEs Generated During 2014-15</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">19</td>
+            <td align="left" valign="top">  <a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement19.xls" target="_blank">Internal Resources Utilized as % of Fixed Assets as on 31.3.2015</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">20</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement20.xls" target="_blank"> Foreign Exchange Earnings of CPSEs in 2014-15</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">21</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement21.xls" target="_blank"></a>Number of Employees and House Constructed in CPSEs as on 31.3.2015</a></td>
+        </tr>
+         <tr>
+          <td align="left" valign="top">22</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement22.xls" target="_blank"> Township Maintenance Expenditure in CPSEs (Cognate Group Wise)</a></td>
+        </tr>   
+        <tr>
+          <td align="left" valign="top">23</td>
+            <td align="left" valign="top"> <a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement23.xls" target="_blank">Break up of Total Employees as on 31-3-2014</a></td>
+        </tr>
+        <tr>
+          <td align="left" valign="top">24</td>
+            <td align="left" valign="top"><a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement24.xls" target="_blank">Representation of SC/ST and OBC in CPSEs as on 31-3-2014 </a></td>
+        </tr>
+        <tr>
+          <td align="left" valign="top">25</td>
+            <td align="left" valign="top">
+	    <a href="<?php print $base_path ?>/sites/upload_files/dpe/files/pesurvey_14_15/vol1/E1Statement25.xls" target="_blank">Equity Participation by Under Construction CPSEs as on 31-3-2014</a></td>
+        </tr>
+        
+          
+        
+      </tbody>
+    </table>
